b687a8987e52d917afc952564fa74a81d516468e
LeonardoSilva <apx88.0@gmail.com>
2020-10-28 16:42:40 -0400

    gyro test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
+const shoe = document.querySelector(".shoe img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
@@ -19,20 +19,29 @@ container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   // Popout
   title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  shoe.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
   sizes.style.transform = "translateZ(100px)";
   purchase.style.transform = "translateZ(75px)";
 });
 
-// Animate Out
+// Reset
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   // Popback
   title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  shoe.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
   sizes.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
 });
+
+let gyroscope = new Gyroscope({frequency: 60});
+
+gyroscope.addEventListener('reading', e => {
+  console.log("Angular velocity along the X-axis " + gyroscope.x);
+  console.log("Angular velocity along the Y-axis " + gyroscope.y);
+  console.log("Angular velocity along the Z-axis " + gyroscope.z);
+});
+gyroscope.start();
